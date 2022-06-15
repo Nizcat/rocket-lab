b9fd80b74d930ef2e013203704ab43ff83aad65a
Nizcat <nizalli@gmail.com>
2022-06-14 19:50:06 -0500

    implementa imagen de fondo y texto sin formato

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { CapStatus } from './CapStatus';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <header className='Initial' >
+          <div >
+            <h1 className='RocketTitle' >
+              <span>ROCKET</span>
+              <span>Improving all the time</span>
+            </h1>
+          </div>
+        </header>
+        <CapStatus />
+      </body>
     </div>
   );
 }
---
+++ src/CapStatus.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const CapStatus = () => {
+    return (
+        <section>
+            <h1> Capsules Status :</h1>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </section>
+    )
+}

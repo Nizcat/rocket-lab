10bb88182f2fbaca5b7de4771f583ae3fb67bb0d
Nizcat <nizalli@gmail.com>
2022-06-18 09:19:50 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { CapStatus } from './CapStatus';
 import { AllCapsules } from './AllCapsules';
+import   rocket  from './assets/rocket.svg'
 import styles from './App.module.css';
 
 function App() {
   return (
     <div className={styles.App}>
       <header className={styles.Initial} >
+        <img className={styles.rocketLogo} src={rocket} alt="rocket logo"/>
         <div className={styles.titles}>
           <h1 className={styles.RocketTitle} >ROCKET</h1>
           <h2 className={styles.RocketSubtitle} >IMPROVING ALL THE TIME</h2>
